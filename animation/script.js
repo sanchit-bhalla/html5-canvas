279c1e51e934f50d7ec4830189e46e060ef8e976
Sanchit Bhalla <sanchitbhalla91@gmail.com>
2025-07-12 18:11:58 +0530

    Adding interactivity to the animation

--- animation/script.js
+++ animation/script.js
@@ -5,6 +5,30 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // canvas.style.backgroundColor = "#000";
 
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+// const colors = ["#e63946", "#f1faee", "#a8dadc", "#457b9d", "#1d3557"];
+const colors = ["#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"];
+
+const circleArr = [];
+const maxRadius = 40;
+const minRadius = 3;
+
+// Resize canvas on resizing browser
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+});
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+
 class AnimatedCircle {
   constructor(x, y, dx, dy, radius) {
     this.x = x;
@@ -12,43 +36,61 @@ class AnimatedCircle {
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = Math.random() * minRadius + 1; // [1, minRadius+1)
+    this.color = colors[Math.floor(Math.random() * colors.length)];
   }
 
-  create() {
+  draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.strokeStyle = "green";
+    // ctx.strokeStyle = "green";
     ctx.stroke();
+    ctx.fill();
+    ctx.fillStyle = this.color;
   }
 
-  animate() {
-    this.create();
-    this.x += this.dx;
-    this.y += this.dy;
-
+  update() {
     if (this.x + this.radius > innerWidth || this.x - this.radius < 0)
       this.dx = -this.dx;
 
     if (this.y + this.radius > innerHeight || this.y - this.radius < 0)
       this.dy = -this.dy;
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    // interactivity
+    // increase size of circles in the viscinity of the the mouse(cursor)
+    if (
+      Math.abs(mouse.x - this.x) < 50 &&
+      Math.abs(mouse.y - this.y) < 50 &&
+      this.radius < maxRadius
+    )
+      this.radius += 1;
+    else if (this.radius > this.minRadius) this.radius -= 1;
+
+    this.draw();
   }
 }
 
-const circleArr = [];
-for (let i = 0; i < 100; i++) {
-  let radius = 30;
-  // convert Math.random i.e [0, 1) into [radius, innerWidth - radius)
-  let x = Math.random() * (innerWidth - 2 * radius) + radius;
-  let y = Math.random() * (innerHeight - 2 * radius) + radius;
-  let dx = (Math.random() - 0.5) * 6;
-  let dy = (Math.random() - 0.5) * 6;
-  circleArr.push(new AnimatedCircle(x, y, dx, dy, radius));
+function init() {
+  circleArr.length = 0;
+  for (let i = 0; i < 800; i++) {
+    let radius = Math.random() * 5 + minRadius;
+    // convert Math.random i.e [0, 1) into [radius, innerWidth - radius)
+    let x = Math.random() * (innerWidth - 2 * radius) + radius;
+    let y = Math.random() * (innerHeight - 2 * radius) + radius;
+    let dx = Math.random() - 0.5;
+    let dy = Math.random() - 0.5;
+    circleArr.push(new AnimatedCircle(x, y, dx, dy, radius));
+  }
 }
 
 function animate() {
   ctx.clearRect(0, 0, innerWidth, innerHeight);
-  circleArr.forEach((circle) => circle.animate());
+  circleArr.forEach((circle) => circle.update());
   requestAnimationFrame(animate);
 }
 
+init();
 animate();
